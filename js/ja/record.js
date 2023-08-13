dcc87fdad210574ba8cb5729daf30060cced83b0
gmy704 <141038562+gmy704@users.noreply.github.com>
2023-08-13 13:15:15 +0900

    Update record.js

--- js/ja/record.js
+++ js/ja/record.js
@@ -1,14 +1,14 @@
-var table = document.getElementById("recording");
+var table = document.getElementById("recording"); //スコア一覧の取得
 
 var records = localStorage.getItem("record");
-var records_read = JSON.parse(records);
 
-var rec_clean = records.replace(/{"play_time":"|]|}|\[|"point":|"/g, '');
-var arr_recode = rec_clean.split(',');
+var rec_clean = records.replace(/{"play_time":"|]|}|\[|"point":|"/g, ''); //一覧を整頓
+var arr_recode = rec_clean.split(','); //配列にする
 
 var cell = 0;
 
 while (cell < arr_recode.length) {
+	//表にスコアを挿入
 	var tr_recording = table.tBodies[0].insertRow(-1);
 	var td_recording = tr_recording.insertCell(-1);
 	td_recording.appendChild(document.createTextNode(arr_recode[cell]));
@@ -17,3 +17,20 @@ while (cell < arr_recode.length) {
 	td_recording.appendChild(document.createTextNode(arr_recode[cell]));
 	cell++;
 }
+
+function savepic(dataurl) {
+	const name = "scores.png";
+	const a = document.createElement("a");
+	a.href = dataurl;
+	a.download = name;
+	a.click();
+}
+
+var capture = document.getElementById("capture");
+capture.addEventListener("click", function(){
+	html2canvas(document.getElementById("recording"), {
+		width: 1200
+	}).then(function(canvas) {
+		savepic(canvas.toDataURL());
+	});
+});
