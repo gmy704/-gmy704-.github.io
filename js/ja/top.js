33b3539aa5dd8d765983c0a54888a2b05f43a19c
gmy704 <141038562+gmy704@users.noreply.github.com>
2023-12-23 13:28:11 +0900

    Delete js directory

--- js/ja/top.js
+++
@@ -1,8 +0,0 @@
-if (localStorage.hasOwnProperty("update")) {
-} else if (localStorage.hasOwnProperty("record")) {
-	localStorage.removeItem("record");
-	localStorage.setItem("update", "1");
-}
-else {
-	localStorage.setItem("update", "1");
-}
